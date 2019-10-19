431eabc82ba475d98fc322abaa62d4e15f30816b
Bentley Davis <Bentley@BentleyDavis.com>
2019-10-19 17:00:09 -0500

    pro based on view

--- src/ClaimElement.jsx
+++ src/ClaimElement.jsx
@@ -10,21 +10,32 @@ class ClaimElement extends Component {
         super(props);
         this.score = props.repository.getScoreBySourceClaimId(props.claimId)
         this.claim = props.repository.getItem(props.claimId)
-        this.children = props.repository.getClaimEdgesByParentId(props.claimId)
+        this.childClaimEedges = props.repository.getClaimEdgesByParentId(props.claimId)
         //const calcInitator = props.calcInitator;
+        this.polarity = this.props.polarityContext;
+        if (this.props.claimEdge !== undefined && this.props.claimEdge.pro === false ) {
+            this.polarity = !this.polarity;
+        } else {
+            //this.polarity = false;
+        }
+        this.pro = this.props.claimEdge !== undefined && (this.props.claimEdge.pro ? "pro" : "con") + " | "
     }
 
     render() {
         return (
             <div className={'claim-outer'}>
+                ({this.polarity ? "pro" : "con"}) |&nbsp;
+                {this.pro}
                 {this.score.confidence} | {this.claim.content}
                 <ul>
-                    {this.children.length > 0 && this.children.map((child) => (
-                        <li>
+                    {this.childClaimEedges.length > 0 && this.childClaimEedges.map((child) => (
+                        <li key={child.childId}>
                             <ClaimElement
                                 claimId={child.childId}
                                 repository={this.props.repository}
                                 calculationInitator={this.props.calculationInitator}
+                                claimEdge={child}
+                                polarityContext={this.polarity}
                             />
                         </li>
                     ))}
