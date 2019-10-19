1ab376581bcb764024e9332338f786544d00a1bd
Bentley Davis <Bentley@BentleyDavis.com>
2019-10-19 15:22:47 -0500

    more complex example

--- src/index.js
+++ src/index.js
@@ -13,16 +13,22 @@ const repo = new Repository();
 const calculationInitator = new CalculationInitator(repo);
 const measuredClaim = new Claim("Measured Claim", ID("measuredClaim"));
 const childClaim1 = new Claim("Child Claim 1", ID("childClaim1"));
-//const childClaim2 = new Claim("Child Claim 2", ID("childClaim2"));
-const descendantClaim = new Claim("Descendant Claim", ID("descendantClaim"));
+const childClaim2 = new Claim("Child Claim 2", ID("childClaim2"));
+const childClaim3 = new Claim("Child Claim 3", ID("childClaim3"));
+const descendantClaim1 = new Claim("Descendant Claim 1", ID("descendantClaim1"));
+const descendantClaim2 = new Claim("Descendant Claim 2", ID("descendantClaim2"));
 calculationInitator.notify([
   new Change(measuredClaim),
   new Change(childClaim1),
-  //new Change(childClaim2),
-  new Change(descendantClaim),
+  new Change(childClaim2),
+  new Change(childClaim3),
+  new Change(descendantClaim1),
+  new Change(descendantClaim2),
   new Change(new ClaimEdge(measuredClaim.id, childClaim1.id, Affects.Confidence, false)),
-  //new Change(new ClaimEdge(measuredClaim.id, childClaim2.id, Affects.Confidence, true)),
-  new Change(new ClaimEdge(childClaim1.id, descendantClaim.id, Affects.Confidence, false)),
+  new Change(new ClaimEdge(measuredClaim.id, childClaim2.id, Affects.Confidence, true)),
+  new Change(new ClaimEdge(measuredClaim.id, childClaim3.id, Affects.Confidence, false)),
+  new Change(new ClaimEdge(childClaim1.id, descendantClaim1.id, Affects.Confidence, false)),
+  new Change(new ClaimEdge(childClaim1.id, descendantClaim2.id, Affects.Confidence, true)),
 ]);
 
 for (const claim of claims) {
