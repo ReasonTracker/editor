1a66ec9d22fa4c2170dd92bc2477c1b51d2f7131
Bentley Davis <Bentley@BentleyDavis.com>
2021-08-10 19:32:56 -0500

    Search Works Better

--- src/SearchElement.tsx
+++ src/SearchElement.tsx
@@ -4,28 +4,40 @@ import { Claim, RepositoryLocalPure, RsData, Score } from "@reasonscore/core";
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import './SearchElement.scss';
 //@ts-ignore
-import { Index } from 'flexsearch';
+import Fuse from 'fuse.js'
 //@ts-ignore
 import { encode } from "../node_modules/flexsearch/dist/module/lang/latin/extra.js";
 import { selectElement } from './selectElement';
+import { resourceUsage } from 'process';
 
 type MyProps = {
     repository: RepositoryLocalPure,
-    mainScoreId: string;
+    mainScoreId: string,
 };
 
-const index = new Index({
-    encode: encode,
-    tokenize: "full",
-    minlength: 2,
-    filter: [],
+const SearchIndex = new Fuse([], {
+    // isCaseSensitive: false,
+    includeScore: true,
+    shouldSort: true,
+    includeMatches: true,
+    findAllMatches: false,
+    minMatchCharLength: 3,
+    // location: 0,
+    // threshold: .2,
+    // distance: 100,
+    // useExtendedSearch: false,
+    ignoreLocation: true,
+    // ignoreFieldNorm: false,
+    keys: [
+        "content",
+    ]
 });
 
 let indexPopulated = false;
 
 const SearchElement = ({ repository, mainScoreId }: MyProps) => {
     const [searchText, setSearchText] = useState("");
-    const [foundClaims, setResults] = useState<{ claim: Claim, score: Score }[]>([]);
+    const [foundClaims, setFoundClaims] = useState<{ claim: Claim, score: Score, searchResult: any }[]>([]);
 
     const handleText = async (e: React.FormEvent<HTMLInputElement>) => {
         const searchText = e.currentTarget.value;
@@ -37,7 +49,8 @@ const SearchElement = ({ repository, mainScoreId }: MyProps) => {
             for (const score of (await repository.getDescendantScoresById(mainScoreId))) {
                 const claim = await repository.getClaim(score.sourceClaimId);
                 if (claim) {
-                    index.add(claim.id, claim.content);
+                    // @ts-ignore
+                    SearchIndex.add({ id: claim.id, content: claim.content })
                 }
             }
         }
@@ -45,13 +58,20 @@ const SearchElement = ({ repository, mainScoreId }: MyProps) => {
 
         // Perform that search
         if (searchText.length > 2) {
-            const items: { claim: Claim, score: Score }[] = [];
-            for (const claimId of index.search(searchText)) {
-                const claim = await repository.getClaim(claimId);
-                const score = (await repository.getScoresBySourceId(claimId))[0];
-                if (claim && score) items.push({ claim: claim, score: score });
+            const items: { claim: Claim, score: Score, searchResult: any }[] = [];
+            const searchResults: any[] = SearchIndex.search({ content: searchText });
+            console.log(searchResults);
+            for (const searchResult of searchResults) {
+                const claim = await repository.getClaim(searchResult.item.id);
+                const score = (await repository.getScoresBySourceId(searchResult.item.id))[0];
+                if (claim && score && searchResult.score < .85) {
+                    items.push({ claim: claim, score: score, searchResult: searchResult });
+                }
             }
-            setResults(items);
+            console.log(items);
+            setFoundClaims(items);
+        } else if (foundClaims.length) {
+            setFoundClaims([]);
         }
     }
 
@@ -84,17 +104,18 @@ const SearchElement = ({ repository, mainScoreId }: MyProps) => {
             <div className={"search-results-hider" + (!foundClaims.length ? "" : " hidden")}>
                 <div className="searchResults">
                     <TransitionGroup component={null}>
-                        {
-                            foundClaims.map(({ claim, score },index) => (
+                        {foundClaims &&
+                            foundClaims.map(({ claim, score, searchResult }, index) => (
                                 <CSSTransition
                                     key={claim.id}
                                     timeout={500}
                                     classNames='searchitem'>
                                     <div className="search-result" onClick={() => handleOpenButtonClick(score.id)}>
-                                        {index? <hr></hr>:""}
+                                        {index ? <hr></hr> : ""}
                                         <span className={'rs-content'} dangerouslySetInnerHTML={createMarkup(claim, score)}></span>
                                         {/* <br></br><a href={getScoreUrl(score)} target="_blank">Open this claim in a new window</a> */}
                                         &nbsp;<a className="searchMoreInfo" onClick={() => handleOpenButtonClick(score.id)} target="_blank">More Info&hellip;</a>
+                                        &nbsp;&nbsp;<small style={{ opacity: .2 }}>{100-Math.round(searchResult.score * 100)}%</small>
                                     </div>
                                 </CSSTransition>
                             ))}
